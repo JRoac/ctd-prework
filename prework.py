c42b3e51f78f1b81b0e227b8e455036a52346c52
JRoac <75328093+JRoac@users.noreply.github.com>
2026-07-10 00:20:05 -0400

    Create prework.py

---
+++ prework.py
@@ -0,0 +1,13 @@
+# This script rates a basketball players free throw shooting percentage.
+
+player_name = 'Stephen Curry'
+ftshooting_percent = 71
+
+if ftshooting_percent >= 88:
+    print(player_name,'great shooting with', ftshooting_percent,'%.')
+    
+elif ftshooting_percent >= 70:
+    print(player_name,'solid shooting with', ftshooting_percent,'%.')
+
+else:
+    print(player_name,'go practice!')
